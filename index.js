d0063238698fc37b8b6a420396c1e035e77b7794
Luca Franceschini <luca.franceschini92@gmail.com>
2017-12-04 15:14:56 +0100

    refactoring and support for 'this' binding

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 'use strict';
 
-// default hook parameter
+// default hook, do nothing
 function emptyHook() { }
 
-// wrap a given function in a new one always invoking pre and postHook
-// this is a nice alias when doing wrap = require('wrapper-roo')
+/*
+ * Wrap a given function in a new one always invoking pre- and post-hook.
+ * This is a nice alias when doing wrap = require('wrapper-roo').
+ * Both arrows and functions are fine here, no 'this' used.
+ */
 exports.aFunction = (func, preHook = emptyHook, postHook = emptyHook) => {
   // arguments checking
   if (typeof func !== 'function')
-    throw new TypeError('The function to be wrapped must be a Function')
+    throw new TypeError('The function to be wrapped must be a function')
 
   if (typeof preHook !== 'function')
-    throw new TypeError('Prehook must be a Function')
+    throw new TypeError('Prehook must be a function')
 
   if (typeof postHook !== 'function')
-    throw new TypeError('PostHook must be a Function')
+    throw new TypeError('Posthook must be a function')
 
   // return the wrapped function
-  return (...args) => {
+  // do not use an arrow here, traditional 'this' binding needed (see below)
+  return function (...args) {
+    preHook()
+
     // try-catch needed to invoke postHook if func throws
     try {
-      preHook()
-      const result = func.apply(this, args)
+      // check if this is a constructor call or not, and do the same
+      // new.target undefined only in constructor call (ES5)
+      const result = new.target
+        ? new func(...args)
+        // forward 'this' binding
+        : func.apply(this, args)
+
       postHook()
       return result
     } catch (err) {
