7bf01f8e4d1d9522bb0db54a8ad89042c35a7a4a
Luca Franceschini <luca.franceschini92@gmail.com>
2018-02-09 19:03:43 +0100

    custom hook and wrapped func

--- test/argumentsAndResult.js
+++ test/argumentsAndResult.js
@@ -3,12 +3,20 @@
 const { nop, wrap } = require('./setup')
 
 describe('arguments and result checking', function () {
-  it('should throw if preHook is not a function', function () {
+  it('should throw if object to be wrapped is not a function', function () {
+    (() => wrap.the('shit')).should.throw(TypeError)
+  })
+
+  it('should throw if pre-hook is not a function', function () {
     (() => wrap(nop).withPreHook('hey')).should.throw(TypeError)
   })
 
-  it('should throw if postHook is not a function', function () {
+  it('should throw if post-hook is not a function', function () {
     (() => wrap(nop).withPostHook('ho')).should.throw(TypeError)
+  })
+
+  it('should throw if custom hook is not a function', function () {
+    (() => wrap(nop).withHook("let's go")).should.throw(TypeError)
   })
 
   it('should return a function', function () {
