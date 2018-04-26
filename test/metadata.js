4a02e25a8fc80a70663294be578d283ba168b0af
Luca Franceschini <luca.franceschini92@gmail.com>
2018-04-26 16:37:44 +0200

    metadata tests

---
+++ test/metadata.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { nop, wrap } = require('./setup')
+const InvocationMetadata = require('../lib/metadata')
+
+describe('Function invocation metadata', function () {
+  it('should throw on non-function objects', function () {
+    (() => new InvocationMetadata(null, [])).should.throw(TypeError)
+  })
+
+  it('should throw on non-array arguments', function () {
+    (() => new InvocationMetadata(() => {}, null)).should.throw(TypeError)
+  })
+
+  it('should contain the original function', function () {
+    function hook (func, metadata) {
+      metadata.function.should.equal(nop)
+    }
+
+    wrap(nop).withCustomHook(hook)()
+  })
+
+  it('should contain the correct arguments')
+})
